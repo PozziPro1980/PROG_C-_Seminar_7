f9009ff70bbb7844424ae22095516929b7155755
Yuri <zver22221980@gmail.com>
2022-12-03 17:46:51 +0700

    ДЗ№ 3. семинар_7.

--- Seminar_7_dz_3/Program.cs
+++ Seminar_7_dz_3/Program.cs
@@ -28,9 +28,9 @@ Console.WriteLine("__________________");
 
 double sum = 0;
 
-for (int i = 0; i < array.GetLength(0); i++)
+for (int j = 0; j < array.GetLength(1); j++)
 {
-   for (int j = 0; j < array.GetLength(1); j++)
+   for (int i = 0; i < array.GetLength(0); i++)
    {
       sum += array[i,j];
    } 
